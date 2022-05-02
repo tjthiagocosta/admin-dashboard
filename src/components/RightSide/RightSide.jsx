f52adfe6ff83180e65d6e890cafbc2f5ca951a76
Thiago Costa <tjthiagocosta@gmail.com>
2022-05-01 22:31:02 -0300

    add Updates and Costumer Review

---
+++ src/components/RightSide/RightSide.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import CustomerReview from "../CostumerReview/CostumerReview";
+import Updates from "../Updates/Updates";
+import "./RightSide.css";
+
+const RightSide = () => {
+  return (
+    <div className="RightSide">
+      <div>
+        <h3>Updates</h3>
+        <Updates />
+      </div>
+      <div>
+        <h3>Customer Review</h3>
+        <CustomerReview />
+      </div>
+    </div>
+  );
+};
+
+export default RightSide;
